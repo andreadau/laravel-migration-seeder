9e06f7596fefe95b4a1e692ec0dc463df81bb670
Andrea <dauriaandrea99@gmail.com>
2021-02-22 17:18:33 +0100

    Employee DB Seeder by Array ADD

--- database/seeds/EmployeeSeeder.php
+++ database/seeds/EmployeeSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Employee;
 use Illuminate\Database\Seeder;
 
 class EmployeeSeeder extends Seeder
@@ -11,6 +12,40 @@ class EmployeeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $employees_list = [
+            [
+                'fullname' => 'Marco Cerri',
+                'company' => 'Lambda',
+                'role' => 'Accountant',
+                "salary" => 23400
+            ],
+            [
+                'fullname' => 'Luca Ambrosini',
+                'company' => 'Delta',
+                'role' => 'Accountant',
+                "salary" => 25423
+            ],
+            [
+                'fullname' => 'Alessandro Bianchi',
+                'company' => 'Gamma',
+                'role' => 'Architect',
+                "salary" => 35000
+            ],
+            [
+                'fullname' => 'Astaldo Almneti',
+                'company' => 'Alpha',
+                'role' => 'Manager',
+                "salary" => 54000
+            ],
+        ];
+
+        foreach ($employees_list as $employee) {
+            $newEmployee = new Employee();
+            $newEmployee->fullname = $employee['fullname'];
+            $newEmployee->company = $employee['company'];
+            $newEmployee->role = $employee['role'];
+            $newEmployee->salary = $employee['salary'];
+            $newEmployee->save();
+        }
     }
 }
