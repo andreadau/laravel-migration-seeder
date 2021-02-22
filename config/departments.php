a5f8fe7529eaaa97da5e5b4405789c1109310e7d
Andrea <dauriaandrea99@gmail.com>
2021-02-22 17:33:04 +0100

    Department Seeder with Config ADD

---
+++ config/departments.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    [
+        'name' => 'Lambda Italy',
+        'company' => 'Lambda',
+        'location' => 'Italy',
+        "utility" => "Advocacy"
+    ],
+    [
+        'name' => 'Delta France',
+        'company' => 'Delta',
+        'location' => 'France',
+        "utility" => 'Accountant'
+    ],
+    [
+        'name' => 'Gamma France',
+        'company' => 'Gamma',
+        'location' => 'France',
+        "utility" => 'Menagment'
+    ],
+    [
+        'name' => 'Alpha Germany',
+        'company' => 'Alpha',
+        'location' => 'Germany',
+        "utility" => 'Accountant'
+    ],
+    [
+        'name' => 'Beta Belgium',
+        'company' => 'Beta',
+        'location' => 'Belgium',
+        "utility" => 'Main Building'
+    ],
+    [
+        'name' => 'Epsilon Czech',
+        'company' => 'Epsilon',
+        'location' => 'Czech Republic',
+        "utility" => 'Menagment'
+    ],
+    [
+        'name' => 'Ypsilon Spain',
+        'company' => 'Ypsilon',
+        'location' => 'Spain',
+        "utility" => 'Advocacy'
+    ],
+];
+
+?>
--- database/seeds/DepartmentSeeder.php
+++ database/seeds/DepartmentSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Department;
 use Illuminate\Database\Seeder;
 
 class DepartmentSeeder extends Seeder
@@ -11,6 +12,14 @@ class DepartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $departments_list = config('departments');
+        foreach ($departments_list as $department) {
+            $newDepartment = new Department();
+            $newDepartment->name = $department['name'];
+            $newDepartment->company = $department['company'];
+            $newDepartment->location = $department['location'];
+            $newDepartment->utility = $department['utility'];
+            $newDepartment->save();
+        }
     }
 }
